6355af38b6c46b28c46c3169dbf8f0fe2fb5acce
keranenkirill <keranen.kirill@gmail.com>
2023-11-03 11:36:17 +0200

    1.4: kurssitiedot step4

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -1,29 +1,28 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-const total ={
-    total_exercises: part1.exercises + part2.exercises + part3.exercises
-  } 
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1.name} exercises1={part1.exercises}/>
-      <Content part2={part2.name} exercises2={part2.exercises}/>
-      <Content part3={part3.name} exercises3={part3.exercises}/>
-<Total total={total.total_exercises}/>
+      <Content part1={parts[0].name} exercises1={parts[0].exercises}/>
+      <Content part2={parts[1].name} exercises2={parts[1].exercises}/>
+      <Content part3={parts[2].name} exercises3={parts[2].exercises}/>
+      <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
     </div>
   )
 }
