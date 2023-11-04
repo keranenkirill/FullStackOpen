84eb0935d9154703771c96aa234f718a76e27060
keranenkirill <keranen.kirill@gmail.com>
2023-11-04 17:54:23 +0200

    1.6: unicafe step1

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -1,40 +1,53 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
+import ReactDom from 'react-dom'
 
-//Laskimen arvon näyttävä komponentti
-const Display = ({ counter }) => <div>{counter}</div>
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
+const Display = (props) => (
+  <div>
+    <span style={{ fontWeight: 'bold' }}>{props.value}</span>
+  </div>
+);
+
+const Button = (props) => (
+  <button
+  onClick={props.handleClick}
+  style={{
+    backgroundColor: 'white',
+    color: 'black',
+    borderRadius: '6px', // Adjust the radius as needed
+    padding: '1px 5px', // Adjust padding as needed
+    cursor: 'pointer',
+    }}
+    >
+    {props.text}
   </button>
-)
+);
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
-  console.log('rendering with counter value', counter)
+  const [good_val, setGoodValue] = useState(0);
+  const [neutral_val, setNeutralValue] = useState(0);
+  const [bad_val, setBadValue] = useState(0);
+  const g_feedback = "GIVE FEEDBACK";
+  const stats = "STATISTICS";
 
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
-  }
-
-  const decreaseByOne = () => { 
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
+  const setToValue = (value, setter) => {
+    console.log('value now', value);
+    setter(value);
+  };
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button handleClick={increaseByOne} text="plus" />
-      <Button handleClick={setToZero} text="zero" />
-      <Button handleClick={decreaseByOne} text="minus" />
+      <Display value={g_feedback} />
+      <Button handleClick={() => setToValue(good_val + 1, setGoodValue)} text="good" />
+      <Button handleClick={() => setToValue(neutral_val + 1, setNeutralValue)} text="neutral" />
+      <Button handleClick={() => setToValue(bad_val + 1, setBadValue)} text="bad" />
+      <p></p>
+      <Display value={stats} />
+      <Display value={`good: ${good_val}`} />
+      <Display value={`neutral: ${neutral_val}`} />
+      <Display value={`bad: ${bad_val}`} />
     </div>
-  )
-} 
+  );
+};
+
 export default App
