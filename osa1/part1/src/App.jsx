2f928870560404dc5f83d689a6562f2f066364ac
keranenkirill <keranen.kirill@gmail.com>
2023-11-04 13:16:02 +0200

    kasitelty aiheet osa1(a-c)

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -1,76 +1,40 @@
+import { useState } from 'react'
+
+//Laskimen arvon näyttävä komponentti
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]  
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
   }
-  const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
 
+  const decreaseByOne = () => { 
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content part1={course.parts[0].name} exercises1={course.parts[0].exercises}/>
-      <Content part2={course.parts[1].name} exercises2={course.parts[1].exercises}/>
-      <Content part3={course.parts[2].name} exercises3={course.parts[2].exercises}/>
-      <Total total={totalExercises}/>
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text="plus" />
+      <Button handleClick={setToZero} text="zero" />
+      <Button handleClick={decreaseByOne} text="minus" />
     </div>
   )
-}
-
-
-
-const Header = (props) =>{
-//Protip: voit kohdata ohjelmoidessasi ongelmia sen suhteen missä muodossa komponentin saamat propsit ovat.
-  // Hyvä keino varmistua asiasta on tulostaa propsit konsoliin
-  console.log(props)
-  return (
-    <div>
-      <p>
-        {props.course.name}
-      </p>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
-    </div>
-  );
-}
-
-const Total = (props) =>{
-  console.log(props)
-  return (
-    <div>
-      <p>
-       Total count of exercises: {props.total}
-      </p>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  );
-}
+} 
 export default App
