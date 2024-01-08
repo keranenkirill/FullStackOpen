d5c1cfb418245b8b18f26deea71fef8489abc242
keranenkirill <keranen.kirill@gmail.com>
2024-01-08 14:16:05 +0200

    App.jsx and main.jsx

--- osa1/src/App.jsx
+++ osa1/src/App.jsx
@@ -4,54 +4,31 @@ import ReactDom from 'react-dom'
 const anecdotes_points = {}
 
 const Button = (props) => (
-  <button onClick={props.handleClick}>{props.text}</button>
+  <button 
+    onClick={props.handleClick}
+    style={{
+      backgroundColor: 'white',
+      color: 'black',
+      borderRadius: '6px', 
+      padding: '1px 5px', 
+      cursor: 'pointer',
+      }}
+      >
+      {props.text}</button>
 );
 
 const DisplayText = (props) =>{
   return(
-    <p>{props.anecdoteline}</p>
-  )
-}
-
-const StatisticsLine = (props) =>{
-  return(
-  <tr>
-    <td>{props.text}</td>
-    <td>{props.val}</td>
-  </tr>
-  )
-}
-
-const Statistics = (props) => {
-  console.log(props) 
-  console.log(anecdotes_points)
-  return (
     <div>
-      <table>
-      <tbody>
-        <StatisticsLine text="good"  val={props.good_val}/>
-        <StatisticsLine text="neutral" val={props.neutral_val}/>
-        <StatisticsLine text="bad" val={props.bad_val}/>
-        <StatisticsLine text="all" val={props.all_votes}/>
-        <StatisticsLine text="average" val={props.average || 0}/>
-        <StatisticsLine text="positive" val={props.pos_pers + " %" || 0}/>
-      </tbody>
-    </table>
-      
+      <p>{props.anecdoteline}</p>
+      <p>{props.text} {props.anecdote_p}</p>
     </div>
   )
 }
 
 const App = () => {
   const [good_val, setGoodValue] = useState(0);
-  const [neutral_val, setNeutralValue] = useState(0);
-  const [bad_val, setBadValue] = useState(0);
-  let all_votes = bad_val+neutral_val+good_val
-  let average = (good_val-bad_val)/all_votes
-  let positive_persentage = (good_val/all_votes)*100  
   const [anecdotes, setAnecdoteLine] = useState(0)
-
-  
   let anecdote_lines= [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
@@ -63,31 +40,16 @@ const App = () => {
     'The only way to go fast is to go well.'
   ]
 
-
   return (
     <div>
       <h1>Give feedback</h1>
       <p></p>
       <DisplayText anecdoteline = {anecdote_lines[anecdotes]}/>
+      <DisplayText text="hase votes: " anecdote_p={anecdotes_points[anecdotes]}/>
       <div>
-        <Button handleClick={() => {setGoodValue(good_val + 1), anecdotes_points[anecdotes] = ++anecdotes_points[anecdotes] || 1}}  text="good" />
-        <Button handleClick={() => setNeutralValue(neutral_val + 1)} text="neutral" />
-        <Button handleClick={() => setBadValue(bad_val + 1)} text="bad" />
-      </div>
-      <br/>
-      <div>
+        <Button handleClick={() => {setGoodValue(good_val + 1), anecdotes_points[anecdotes] = ++anecdotes_points[anecdotes] || 1}}  text="vote" />
         <Button handleClick={() => setAnecdoteLine(Math.floor((Math.random() * anecdote_lines.length)))} text="generate anecdote" />
       </div>
-
-
-      
-  
-      <h1>Statistics</h1>
-      { all_votes===0 ? 
-        (<h4> No feedback given </h4> ) 
-        : 
-        (<Statistics  good_val={good_val} neutral_val={neutral_val} bad_val={bad_val}  all_votes={all_votes} average={average} pos_pers={positive_persentage} />)
-      }
     </div>
   );
 };
--- osa1/src/main.jsx
+++ osa1/src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 
 
-import App from './tasks/1_1_step1'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
