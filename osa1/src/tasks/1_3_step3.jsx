239f47d849e151c6704469a17cba2d3161eb4cc4
keranenkirill <keranen.kirill@gmail.com>
2024-01-08 14:05:23 +0200

    1.3: kurssitiedot step3

--- osa1/src/tasks/1_3_step3.jsx
+++ osa1/src/tasks/1_3_step3.jsx
@@ -23,7 +23,7 @@ const App = () => {
        <Content part1={part1.name} exercises1={part1.exercises}/>
        <Content part2={part2.name} exercises2={part2.exercises}/>
        <Content part3={part3.name} exercises3={part3.exercises}/>
- <Total total={total.total_exercises}/>
+       <Total total={total.total_exercises}/>
      </div>
    )
  }
