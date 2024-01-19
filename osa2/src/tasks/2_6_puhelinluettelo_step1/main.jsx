c8c69f0e037a8232218b961bf220a136440414ef
keranenkirill <keranen.kirill@gmail.com>
2024-01-19 15:01:56 +0200

    2.6: puhelinluettelo step1 txt

---
+++ osa2/src/tasks/2_6_puhelinluettelo_step1/main.jsx
@@ -0,0 +1,7 @@
+// Kopioi alla oleva koodi osa2/src/App.jsx tiedostoon, jotta voit ajaa ohjelman
+
+
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
