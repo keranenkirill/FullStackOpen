8bc7efc449c26299d9a0ad4fd6be6df869274067
keranenkirill <keranen.kirill@gmail.com>
2024-02-01 14:39:43 +0200

    2.11: puhelinluettelo step6

--- osa2/src/App.jsx
+++ osa2/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Display = ({ persons }) => {
   return (
@@ -40,44 +41,22 @@ const Form = ({
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-    { name: "John Doe", number: "123-456789" },
-    { name: "Jane Smith", number: "987-654321" },
-    { name: "Alice Johnson", number: "111-222333" },
-    { name: "Bob Williams", number: "555-666777" },
-    { name: "Charlie Davis", number: "999-888777" },
-    { name: "David Moore", number: "444-333222" },
-    { name: "Emily Taylor", number: "777-888999" },
-    { name: "Frank Martin", number: "666-555444" },
-    { name: "Grace Jackson", number: "222-111000" },
-    { name: "Henry Brown", number: "333-444555" },
-    { name: "Ivy Martinez", number: "888-999000" },
-    { name: "Jack Wilson", number: "444-555666" },
-    { name: "Kelly White", number: "111-222333" },
-    { name: "Leo Harris", number: "666-555444" },
-    { name: "Mia Clark", number: "999-888777" },
-    { name: "Nathan Turner", number: "555-666777" },
-    { name: "Olivia Young", number: "111-222333" },
-    { name: "Peter Turner", number: "444-333222" },
-    { name: "Quinn Foster", number: "777-888999" },
-    { name: "Rachel Lee", number: "555-666777" },
-    { name: "Samuel Reed", number: "333-444555" },
-    { name: "Tina Stewart", number: "222-111000" },
-    { name: "Ulysses Brooks", number: "444-333222" },
-    { name: "Victoria Hill", number: "666-555444" },
-    { name: "Walter Phillips", number: "888-999000" },
-    { name: "Xena Fisher", number: "111-222333" },
-    { name: "Yasmine Baker", number: "555-666777" },
-    { name: "Zachary Brown", number: "777-888999" },
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("add new person...");
   const [newPhoneNumber, setNewPhoneNum] = useState("add phonenumber...");
   const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    axios
+    .get("http://localhost:3001/persons")
+    .then((response) => {
+      console.log("promise fulfilled");
+      console.log(response.data);
+      setPersons(response.data);
+    });
+  }, []);
+  console.log("render", persons.length, "persons");
 
   const addPerson = (event) => {
     event.preventDefault();
