a9e1786a3a77b4a9e9137f6e9ef5dfb5aef98c15
keranenkirill <keranen.kirill@gmail.com>
2024-02-25 14:45:07 +0200

    3.9 puhelinluettelon backend step9-11 (frontti)

--- osa2/src/services/numbers.js
+++ osa2/src/services/numbers.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'api/persons'
 //toiminallisuus, joka vastaa palvelimen kanssa kommunikoinnista
 const getAll = () => {
   const request = axios.get(baseUrl)
