79a229274bfaac1fe042c0a8d0dfe618b923fac3
Kirill Keränen <Keranen.kirill@gmail.com>
2024-04-29 18:18:25 +0300

    4.5*: apufunktioita ja yksikkötestejä, step3

--- osa4/backend/tests/list_helper.test.js
+++ osa4/backend/tests/list_helper.test.js
@@ -1,6 +1,8 @@
 const { test, describe } = require("node:test")
 const assert = require("node:assert")
 const listHelper = require("../utils/list_helper")
+const { listWithZeroBlogs, listWithOneBlog, listWithMultipleBlogs } = require("./blogLists_testData")
+
 
 test("dummy returns one", () => {
   const blogs = []
@@ -10,71 +12,6 @@ test("dummy returns one", () => {
 })
 
 describe("total likes", () => {
-  const listWithZeroBlogs = []
-
-  const listWithOneBlog = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0,
-    },
-  ]
-
-  const listWithMultipleBlogs = [
-    {
-      _id: "5a422a851b54a676234d17f7",
-      title: "React patterns",
-      author: "Michael Chan",
-      url: "https://reactpatterns.com/",
-      likes: 7,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0,
-    },
-    {
-      _id: "5a422b3a1b54a676234d17f9",
-      title: "Canonical string reduction",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-      likes: 12,
-      __v: 0,
-    },
-    {
-      _id: "5a422b891b54a676234d17fa",
-      title: "First class tests",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-      likes: 10,
-      __v: 0,
-    },
-    {
-      _id: "5a422ba71b54a676234d17fb",
-      title: "TDD harms architecture",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-      likes: 0,
-      __v: 0,
-    },
-    {
-      _id: "5a422bc61b54a676234d17fc",
-      title: "Type wars",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-      likes: 2,
-      __v: 0,
-    },
-  ]
-
-
   
   test("of empty list is zero", () => {
     const result = listHelper.totalLikes(listWithZeroBlogs)
@@ -88,6 +25,31 @@ describe("total likes", () => {
 
   test("of a bigger list is calculated right", () => {
     const result = listHelper.totalLikes(listWithMultipleBlogs)
-    assert.strictEqual(result, 36)
+    assert.strictEqual(result, 46)
+  })
+})
+
+describe("max likes", () => {
+  test("when list is empty, max likes is null", () => {
+    const result = listHelper.maxLikes(listWithZeroBlogs)
+    assert.strictEqual(result, null)
+  })
+
+  test("when there's only one blog, max likes equals the likes of that blog", () => {
+    const result = listHelper.maxLikes(listWithOneBlog)
+    assert.deepStrictEqual(result, {
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      likes: 5,
+    })
+  })
+
+  test("when there are multiple blogs, max likes returns the blog with the most likes", () => {
+    const result = listHelper.maxLikes(listWithMultipleBlogs)
+    assert.deepStrictEqual(result, {
+      title: "First class tests",
+      author: "Robert C. Martin",
+      likes: 20,
+    })
   })
 })
--- osa4/backend/utils/list_helper.js
+++ osa4/backend/utils/list_helper.js
@@ -6,7 +6,26 @@ const totalLikes = (blogs) => {
   return blogs.reduce((totalLikes, blog) => totalLikes + blog.likes, 0)
 }
 
+
+const maxLikes = (blogs) => {
+  if (blogs.length === 0) return null 
+
+  let maxLikesBlog = blogs[0] 
+  for (const blog of blogs) {
+    if (blog.likes > maxLikesBlog.likes) {
+      maxLikesBlog = blog 
+    }
+  }
+  return {
+    title: maxLikesBlog.title,
+    author: maxLikesBlog.author,
+    likes: maxLikesBlog.likes
+  }
+}
+
+
 module.exports = {
   dummy,
   totalLikes,
+  maxLikes
 }
